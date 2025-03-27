3508a186150dcebf75707df2416a7d1cf0c249e4
Tien Dat Pham <dat20036@gmail.com>
2025-03-27 17:44:07 +0700

    Log useful locations to stdout.

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
-import "rest-client/routes"
+import (
+	"fmt"
+	"rest-client/routes"
+)
 
 func main() {
 	routes.Setup()
+
+	fmt.Println("The server is online @ http://localhost:7554")
+	fmt.Println("OpenAPI is online @ http://localhost:7554/docs")
 }
